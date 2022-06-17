9601b04f68a6a665e0601d5220a6be0d547baa00
DataBase <dev.database137@outlook.com>
2022-06-17 12:58:23 -0700

    Added mobile nav, Updated Sign Up and Login pages.

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,22 +3,24 @@ import { useRouter } from "next/router";
 
 const Navbar = () => {
     const router = useRouter();
-    
+
     return ( 
-        <nav className="nav-container">
-            <div className="nav">
+        <div className="nav-container">
+            <header>
                 <h3 className="logo">WebZone</h3>
-                <ul className="links">
-                    <li>
-                        <Link href="/"><a>Home</a></Link>
-                    </li>
-                    <li>
-                        <Link href="/login"><a>Login</a></Link>
-                    </li>
-                </ul>
-                <button className="sign-up-btn btn" onClick={() => {router.push("/signup")}}><span>Sign Up </span></button>
-            </div>
-        </nav>
+                <input type="checkbox" id="nav-toggle" className="nav-toggle" />
+                <nav>
+                    <ul>
+                        <li><Link href="/"><a>Home</a></Link></li>
+                        <li><Link href="/login"><a>Login</a></Link></li>
+                    </ul>
+                    <button className="sign-up-btn btn" onClick={() => {router.push("/signup")}}><span>START NOW </span></button>
+                </nav>
+                <label htmlFor="nav-toggle" className="nav-toggle-label">
+                    <span></span>
+                </label>
+            </header>    
+        </div>
      );
 }
  
--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,26 @@
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "../styles/Login.module.css";
 import Head from "next/head";
+import Link from "next/link";
 
 const Login = () => {
+    const form = useRef();
+    const [rememberMe, setRememberMe] = useState(false);
+
     useEffect(() => {
-        function handleSubmit(event) {
+        const handleFormSubmit = event => {
             event.preventDefault();
-
             const data = new FormData(event.target);
-
-            const value = Object.fromEntries(data.entries());
-            
-            console.log(value);
+            const value = Object.fromEntries(data.entries())
+            const dataSend = {"name": value.username, "password": value.password}
+            if (value.remember_me === "on") {
+                setRememberMe(true);
+            } else {
+                setRememberMe(false);
+            }
         }
 
-        const form = document.querySelector(".loginForm");
-        form.addEventListener("submit", handleSubmit);
+        form.current.addEventListener("submit", handleFormSubmit);
     }, [])
 
     return (
@@ -27,26 +32,43 @@ const Login = () => {
             <div className={ styles.loginModule }>
                 <div className={ styles.loginEmail }>
                     <h1 className={ styles.emailHeader }>Login</h1>
-                    <form className="loginForm">
+                    <form ref={ form }>
                         <div className={ styles.inputs }>
                             <div className={ styles.textInputDiv }>
                                 <br />
-                                <input className={ styles.textInput } type="email" name="email" id="email" required placeholder=" " autoComplete="no" />
-                                <span className={ styles.floatingLabel }>Email</span>
+                                <input
+                                className={ styles.textInput }
+                                type="username"
+                                name="username"
+                                id="username"
+                                required
+                                placeholder=" "
+                                autoComplete="no" />
+                                <span className={ styles.floatingLabel }><label htmlFor="username">Username</label></span>
                             </div>
                             <div className={ styles.textInputDiv }>
                                 <br />
-                                <input className={ styles.textInput } type="password" name="password" id="password" placeholder=" " required />
-                                <span className={ styles.floatingLabel }>Password</span>
+                                <input
+                                className={ styles.textInput }
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder=" "
+                                required />
+                                <span className={ styles.floatingLabel }><label htmlFor="password">Password</label></span>
                             </div>
                         </div>
                         <div className={ styles.rememberMeCheck }>
                             <label className={ styles.checkLabel }>
-                                <input type="checkbox" name="remember_me" id="remember_me" className={ styles.checkInput } />
+                                <input
+                                type="checkbox"
+                                name="remember_me"
+                                className={ styles.checkInput } />
                                 <span className={ styles.slider }></span>
                             </label>
                             <label className={ styles.remMeLabel }>Remember Me</label>
                         </div>
+                        <p className={ styles.signUpRedirect }>Not a user? <Link href="/login"><a>Sign Up</a></Link></p>
                         <button type="submit" className={ styles.submitButton }>Login</button>                        
                     </form>
                 </div>
